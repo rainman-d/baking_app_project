38332de6db523326f754a485ef6b00dd01f8da94
David Rainey <david.rainey83@gmail.com>
2018-07-23 21:41:21 -0500

    Refactoring

--- app/src/main/java/com/drainey/bakingapp/widget/RecipeIngredientsService.java
+++ app/src/main/java/com/drainey/bakingapp/widget/RecipeIngredientsService.java
@@ -16,8 +16,6 @@ import java.util.List;
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
  */
 public class RecipeIngredientsService extends IntentService {
     private static final String ACTION_UPDATE_INGREDIENTS =
